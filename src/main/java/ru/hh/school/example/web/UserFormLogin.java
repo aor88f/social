9ea0be3cbde072f6ee7b267a24cb23650c790725
Alexey O <aor88f@gmail.com>
2012-01-08 03:59:30 +0400

    Add UserFormLogin logs.

--- src/main/java/ru/hh/school/example/web/UserFormLogin.java
+++ src/main/java/ru/hh/school/example/web/UserFormLogin.java
@@ -1,23 +1,30 @@
 package ru.hh.school.example.web;
 
+import ru.hh.school.example.Logger;
+
 public class UserFormLogin {
 
+  private final Logger logger = new Logger(this);
   private String email;
   private String password;
 
   public String getEmail() {
+    logger.log("getEmail");
     return email;
   }
 
   public void setEmail(String email) {
+    logger.log("setEmail");
     this.email = email;
   }
 
   public String getPassword() {
+    logger.log("getPassword");
     return password;
   }
 
   public void setPassword(String password) {
+    logger.log("setPassword");
     this.password = password;
   }
 }
