c65c197e5bccdba66aa24e8581a05be60a70fbb5
Alexey O <aor88f@gmail.com>
2012-01-08 04:04:53 +0400

    Add User logs

--- src/main/java/ru/hh/school/example/User.java
+++ src/main/java/ru/hh/school/example/User.java
@@ -4,25 +4,30 @@ import ru.hh.school.example.ddd.Entity;
 
 public class User extends Entity {
 
+  private final Logger logger = new Logger(this);
   private String email;
   private String password;
   private String fullName;
 
   public User(String email, String password, String fullName) {
+    logger.log("User");
     this.email = email;
     this.password = password;
     this.fullName = fullName;
   }
 
   public String getEmail() {
+    logger.log("getEmail");
     return email;
   }
 
   public String getPassword() {
+    logger.log("getPassword");
     return password;
   }
 
   public String getFullName() {
+    logger.log("getFullName");
     return fullName;
   }
 }
