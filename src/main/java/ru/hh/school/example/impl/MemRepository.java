ed7a7027d435bb843972371da3b38d7ddf680426
Alexey O <aor88f@gmail.com>
2012-01-08 04:08:45 +0400

    Add MemRepository logs

--- src/main/java/ru/hh/school/example/impl/MemRepository.java
+++ src/main/java/ru/hh/school/example/impl/MemRepository.java
@@ -2,29 +2,36 @@ package ru.hh.school.example.impl;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import ru.hh.school.example.Logger;
 import ru.hh.school.example.ddd.Entity;
 import ru.hh.school.example.ddd.Repository;
 import ru.hh.school.example.exceptions.login.LoginException;
 
 public abstract class MemRepository<T extends Entity> implements Repository<T> {
 
+  private final Logger logger = new Logger(this);
   private final Map<Long, T> identityMap = new HashMap<Long, T>();
   private Long counter = 0L;
 
   public void put(T entity) {
+    logger.log("put");
     entity.setId(++counter);
     identityMap.put(entity.getId(), entity);
   }
 
   public T byId(Long id) {
+    logger.log("byId");
     return identityMap.get(id);
   }
 
   public void remove(T entity) {
+    logger.log("remove");
     identityMap.remove(entity.getId());
   }
 
   public Iterable<T> all() {
+    logger.log("all");
     return identityMap.values();
   }
 }
