dab8ea1942bc1b9cfcef857f6f0d57ac86593bcd
Alexey O <aor88f@gmail.com>
2012-01-09 20:49:59 +0400

    Add testGetUserBySessionId

--- src/main/java/Test/UserFacadeTest.java
+++ src/main/java/Test/UserFacadeTest.java
@@ -11,7 +11,7 @@ public class UserFacadeTest extends junit.framework.TestCase {
   private UserRepository userRepository = new MemUserRepository();
   private UserFacade userFacade = new UserFacade(userRepository, new UserService(userRepository));
 
-  public void testLoginRegister() throws Exception {
+  public void testRegisterLoginLogout() throws Exception {
     final String email         = "asdf@asdf.asdf";
     final String password      = "password";
     final String wrongPassword = "wrong password";
@@ -38,4 +38,43 @@ public class UserFacadeTest extends junit.framework.TestCase {
     sessionUser = userFacade.getUserBySessionId(sessionId);
     assertNull(sessionUser);
   }
+
+    public void testGetUserById() throws Exception {
+        assertNull(userFacade.getUserById(1));
+        assertNull(userFacade.getUserById(2));
+        assertNull(userFacade.getUserById(3));
+
+        userFacade.registerUser("aa@aa.aa", "", "");
+        assertEquals("aa@aa.aa", userFacade.getUserById(1).getEmail());
+        assertNull(userFacade.getUserById(2));
+        assertNull(userFacade.getUserById(3));
+
+        userFacade.registerUser("bb@bb.bb", "", "");
+        assertEquals("aa@aa.aa", userFacade.getUserById(1).getEmail());
+        assertEquals("bb@bb.bb", userFacade.getUserById(2).getEmail());
+        assertNull(userFacade.getUserById(3));
+
+        userFacade.registerUser("cc@cc.cc", "", "");
+        assertEquals("aa@aa.aa", userFacade.getUserById(1).getEmail());
+        assertEquals("bb@bb.bb", userFacade.getUserById(2).getEmail());
+        assertEquals("cc@cc.cc", userFacade.getUserById(3).getEmail());
+    }
+    
+    public void testGetUserBySessionId() throws Exception {
+        assertNull(userFacade.getUserBySessionId("sessionId1"));
+        assertNull(userFacade.getUserBySessionId("sessionId2"));
+
+        userFacade.registerUser("aaa@aa.aa", "", "");
+        userFacade.registerUser("bbb@bb.bb", "", "");
+        userFacade.loginUser("aaa@aa.aa", "", "sessionId1");
+        userFacade.loginUser("bbb@bb.bb", "", "sessionId2");
+
+        assertEquals("aaa@aa.aa", userFacade.getUserBySessionId("sessionId1").getEmail());
+        assertEquals("bbb@bb.bb", userFacade.getUserBySessionId("sessionId2").getEmail());
+
+        userFacade.logoutUser("sessionId1");
+
+        assertNull(userFacade.getUserBySessionId("sessionId1"));
+        assertEquals("bbb@bb.bb", userFacade.getUserBySessionId("sessionId2").getEmail());
+    }
 }
