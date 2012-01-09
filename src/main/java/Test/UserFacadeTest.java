d97ec276894faf7009dd7ab1b7fbd54d55973388
Alexey O <aor88f@gmail.com>
2012-01-09 20:22:00 +0400

    Changes to register and login test

--- src/main/java/Test/UserFacadeTest.java
+++ src/main/java/Test/UserFacadeTest.java
@@ -1,7 +1,9 @@
 package Test;
 
+import ru.hh.school.example.User;
 import ru.hh.school.example.UserRepository;
 import ru.hh.school.example.UserService;
+import ru.hh.school.example.exceptions.login.LoginException;
 import ru.hh.school.example.impl.MemUserRepository;
 import ru.hh.school.example.web.UserFacade;
 
@@ -10,8 +12,30 @@ public class UserFacadeTest extends junit.framework.TestCase {
   private UserFacade userFacade = new UserFacade(userRepository, new UserService(userRepository));
 
   public void testLoginRegister() throws Exception {
+    final String email         = "asdf@asdf.asdf";
+    final String password      = "password";
+    final String wrongPassword = "wrong password";
+    final String sessionId     = "Session ID";
+    final String fullName      = "Full Name";
     assertNull(userFacade.getUserById(1));
-    userFacade.registerUser("asdf@asdf.asdf", "", "");
+    userFacade.registerUser(email, password, fullName);
     assertNotNull(userFacade.getUserById(1));
+    try {
+        userFacade.loginUser(email, wrongPassword, sessionId);
+        assertTrue(false);
+    }
+    catch (LoginException e) {
+    }
+    User sessionUser = userFacade.getUserBySessionId(sessionId);
+    assertNull(sessionUser);
+    userFacade.loginUser(email, password, sessionId);
+    sessionUser = userFacade.getUserBySessionId(sessionId);
+    assertNotNull(sessionUser);
+    assertEquals(email, sessionUser.getEmail());
+    assertEquals(password, sessionUser.getPassword());
+    assertEquals(fullName, sessionUser.getFullName());
+    userFacade.logoutUser(sessionId);
+    sessionUser = userFacade.getUserBySessionId(sessionId);
+    assertNull(sessionUser);
   }
 }
